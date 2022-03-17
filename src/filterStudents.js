bb48d08095ec8647532ff74c89afbef1f89d9a54
Blablage <95836461+Blablage@users.noreply.github.com>
2022-03-17 07:07:30 -0500

    Add files via upload

--- src/filterStudents.js
+++ src/filterStudents.js
@@ -21,6 +21,7 @@ queryData.forEach((student) =>  studentsArray.push(student.data()));
 let searchValue = document.getElementById("searchBox");
 let optionValue = document.getElementById("searchOptions");
 let searchButton = document.getElementById("searchButton");
+let titleSearch = document.getElementById("titleSearch");
 
 //Compara y filtra los datos ingresados segun el parámetro de busqueda elegido
 searchButton.addEventListener("click", results);
@@ -67,6 +68,7 @@ function buildFilteredTable(data) {
     `
 
     table.innerHTML = studentElements;
+    titleSearch.style.display = "block";
     studentTable.style.display = 'table';
   })
    
