e1383c6e50094374c80c64cbc859926a598c71e4
Blablage <95836461+Blablage@users.noreply.github.com>
2022-03-17 06:43:18 -0500

    Add files via upload

---
+++ src/filterStudents.js
@@ -0,0 +1,73 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js"
+import { getFirestore, collection, getDocs, query } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js"
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAbB5YTMV3gkN-VvegI39ogGyeJBo0Ng5I",
+  authDomain: "school-5aab5.firebaseapp.com",
+  projectId: "school-5aab5"
+});
+
+const db = getFirestore(firebaseApp);
+
+const consulta = query(collection(db, 'school'));
+
+let queryData = await getDocs(consulta)
+.catch(e => console.log(e));
+
+//Variable que catura los estudiantes y sus datos de la base de datos
+let studentsArray = [];
+queryData.forEach((student) =>  studentsArray.push(student.data()));
+
+let searchValue = document.getElementById("searchBox");
+let optionValue = document.getElementById("searchOptions");
+let searchButton = document.getElementById("searchButton");
+
+//Compara y filtra los datos ingresados segun el parámetro de busqueda elegido
+searchButton.addEventListener("click", results);
+
+function results() {
+
+  const studentsFiltered = studentsArray.filter(elem => {
+    return (
+      elem[`${optionValue.value}`] === searchValue.value
+    )
+  })
+  buildFilteredTable(studentsFiltered);
+}
+
+//Convierte la marca de tiempo a la fecha en formato dd/mm/aaaa
+function getOnlyDate(timestamp){
+      let date = new Date(timestamp.seconds*1000);
+      return date.toLocaleString('es-MX', { year: 'numeric', month: '2-digit', day: '2-digit'})
+}
+
+// //Genera la tabla filtrada de los alumnos y su informacion desde la base de datos de firebase
+function buildFilteredTable(data) {
+
+  //Variable que guarda la tabla de las busquedas
+  let studentElements = '';
+  let table = document.getElementById('databaseTable');
+  let studentTable = document.getElementById('studentTable');
+
+  data.forEach(elem => {
+
+    studentElements = `
+    ${!!studentElements ? studentElements : ''}
+    <tr>
+      <th scope="row">${elem.id}</th>
+      <td>${elem.name}</td>
+      <td>${elem.course}</td>
+      <td>${elem.grade}</td>
+      <td>${getOnlyDate(elem.startDate)}</td>
+      <td>${getOnlyDate(elem.endDate)}</td>
+      <td class="d-flex">
+        <a class="ps-2 mx-4" href="#"><img src="images/plus.png"/></a>
+      </td>
+    </tr>
+    `
+
+    table.innerHTML = studentElements;
+    studentTable.style.display = 'table';
+  })
+   
+}
