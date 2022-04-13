b8061d3dfd1fa3be6a4c81a7c77316278dcec851
Gerardo Garcia <gergarmac@gmail.com>
2022-04-13 07:35:59 -0500

    Preparando para webpack

---
+++ dist/bundle.js
@@ -0,0 +1,39 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: The top-level-await experiment is not enabled (set experiments.topLevelAwait: true to enabled it)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\nError: The top-level-await experiment is not enabled (set experiments.topLevelAwait: true to enabled it)\\n    at /home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/dependencies/HarmonyDetectionParserPlugin.js:54:11\\n    at Hook.eval [as call] (eval at create (/home/gergarmac/Proyecto-Bedu-v2/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:5:16)\\n    at Hook.CALL_DELEGATE [as _call] (/home/gergarmac/Proyecto-Bedu-v2/node_modules/tapable/lib/Hook.js:14:14)\\n    at JavascriptParser.walkAwaitExpression (/home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/javascript/JavascriptParser.js:2320:29)\\n    at JavascriptParser.walkExpression (/home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/javascript/JavascriptParser.js:2250:10)\\n    at JavascriptParser.walkVariableDeclaration (/home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/javascript/JavascriptParser.js:2104:33)\\n    at JavascriptParser.walkStatement (/home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/javascript/JavascriptParser.js:1598:10)\\n    at JavascriptParser.walkStatements (/home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/javascript/JavascriptParser.js:1459:9)\\n    at JavascriptParser.parse (/home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/javascript/JavascriptParser.js:3353:9)\\n    at /home/gergarmac/Proyecto-Bedu-v2/node_modules/webpack/lib/NormalModule.js:1087:26\");\n\n//# sourceURL=webpack://Proyecto-Bedu-v2/./src/index.js?");
+
+/***/ }),
+
+/***/ "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js":
+false,
+
+/***/ "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js":
+false
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
